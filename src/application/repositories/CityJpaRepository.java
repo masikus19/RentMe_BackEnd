86fa6b9c303dcd98279addf8f2464a46dfbf0f7d
masikus19 <98450089+masikus19@users.noreply.github.com>
2022-01-26 10:54:04 +0200

    Delete src/application directory

--- src/application/repositories/CityJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import application.entities.City;
-
-public interface CityJpaRepository extends JpaRepository<City, Integer>{
-
-}
--- src/application/repositories/CountryJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import application.entities.Country;
-
-public interface CountryJpaRepository extends JpaRepository<Country, String>{
-
-}
